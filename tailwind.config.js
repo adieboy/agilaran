beb1fdbca2b0df189f809b8a58cef1e19dd712ff
Adie <capilitanadie@gmail.com>
2023-08-15 06:04:58 +0800

    FRONT END DONE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities ({
+    '.rotate-y-180': {
+      transform: "rotateY(180deg)"
+    },
+    '.-rotate-y-180': {
+      transform: "rotateY(-180deg)"
+    }
+  })
+})
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
@@ -20,6 +33,6 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [rotateY],
 }
 
